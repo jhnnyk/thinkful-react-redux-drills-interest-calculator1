014ae35c4160faa8008367753b9abd9beb3edcab
John Knotts <jhnnyk@gmail.com>
2017-12-05 15:50:28 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import InterestCalculator from "./components/interest-calculator";
+import "./index.css";
+
+ReactDOM.render(<InterestCalculator />, document.getElementById("root"));
